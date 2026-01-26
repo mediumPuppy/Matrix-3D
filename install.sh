afcb8433774c2e9c877062c2351f695a6211ac6f
Jefferson <83100592+mediumPuppy@users.noreply.github.com>
2026-01-26 10:46:27 -0700

    Fix directory change command in install script

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ python setup.py install
 cd ../..
 pip install git+https://github.com/rmurai0610/diff-gaussian-rasterization-w-pose.git --no-build-isolation
 git clone https://github.com/esw0116/ODGS.git
-cd ../ODGS
+cd ODGS
 pip install submodules/odgs-gaussian-rasterization --no-build-isolation
 
 cd ../..
